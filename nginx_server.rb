1f3ed57df6f1705213e5247738c65866356dc8c1
Rakesh Kirola <55341835+urakesh661@users.noreply.github.com>
2021-03-07 00:13:14 +0530

    Update nginx_server.rb

--- nginx_server.rb
+++ nginx_server.rb
@@ -1,46 +1,32 @@
 control "nginx-server" do                      
   impact 1.0                                
   title "Verify the configuration of nginx server."
-  desc "Verify the configuration of nginx server on centos machine."
+  desc "Verify the configuration of nginx server on centos machine by executing below tests."
 
-  # Verify that nginx is installed
-  
   describe package('nginx') do
     it { should be_installed }
   end
 
-  # Verify that nginx is not running
-
   describe service('nginx') do
     it { should_not be_running }
   end
-
-  # Verify that owner and group is root
 
   describe file('/var/lib/nginx') do
     its('owner') { should eq 'nginx' }
     its('group') { should eq 'root' }
   end  
 
-  # Verify that nginx.conf exists in location /etc/nginx/nginx.conf
-
   describe file('/etc/nginx/nginx.conf') do
     it {should exist}
   end 
-
-  # Verify that size of file fastcgi.conf is greater than 100
 
   describe file('/etc/nginx/fastcgi.conf') do
     its('size') { should be > 100 }
   end    
 
-  # Verify that directory /etc/nginx exists.
-
   describe file('/etc/nginx') do
     it { should be_directory }
   end  
-
-  # Verify that file nginx.conf contains required content.
   
   describe file('/etc/nginx/nginx.conf') do
     it {should exist}
